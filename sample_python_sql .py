1b2ed073df74c3967b3a0bff91ed9adb4293015c
ASWIN KUMAR K <60654719+aswinkumar304@users.noreply.github.com>
2023-04-19 12:04:10 +0530

    Create sample_python_sql .py

---
+++ sample_python_sql .py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="1234",
+  database="sportsanalyst"
+)
+
+mycursor = mydb.cursor()
+#mycursor.execute("CREATE DATABASE sportsanalyst")
+#mycursor.execute("CREATE TABLE CUSTOMER ( NAME VARCHAR(255), ADDRESS VARCHAR(255))")
+#mycursor.execute("SHOW TABLES")
+#mycursor.execute("ALTER TABLE CUSTOMER ADD COLUMN ID INT AUTO_INCREMENT PRIMARY KEY")
+sql ="INSERT INTO CUSTOMER (NAME, ADDRESS) VALUES (%s,%s)"
+val =("aswin","salem")
+mycursor.execute(sql,val)
+mydb.commit()
