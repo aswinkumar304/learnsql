55220f4f0428794a1639850c35dbf9baf6514db8
ASWIN KUMAR K <60654719+aswinkumar304@users.noreply.github.com>
2023-04-19 12:24:39 +0530

    Update sample_python_sql .py

--- sample_python_sql .py
+++ sample_python_sql .py
@@ -1,18 +1,19 @@
-import mysql.connector
 
+import mysql.connector  #helps to connect mysql database using python
+#connect mysql server running on the local machine
 mydb = mysql.connector.connect(
   host="localhost",
   user="****",
   password="****",
   database="sportsanalyst"
 )
-
+#create cursor object it is used to execute sql queries and fetch the data
 mycursor = mydb.cursor()
 #mycursor.execute("CREATE DATABASE sportsanalyst")
 #mycursor.execute("CREATE TABLE CUSTOMER ( NAME VARCHAR(255), ADDRESS VARCHAR(255))")
 #mycursor.execute("SHOW TABLES")
 #mycursor.execute("ALTER TABLE CUSTOMER ADD COLUMN ID INT AUTO_INCREMENT PRIMARY KEY")
-sql ="INSERT INTO CUSTOMER (NAME, ADDRESS) VALUES (%s,%s)"
-val =("aswin","salem")
-mycursor.execute(sql,val)
-mydb.commit()
+edata ="INSERT INTO CUSTOMER (NAME, ADDRESS) VALUES (%s,%s)"
+evals =("aswin","salem")
+mycursor.execute(edata,evals)
+mydb.commit() # commit the current changes
