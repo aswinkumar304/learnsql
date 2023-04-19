21939b9021657169e008ca685326f880bb841665
ASWIN KUMAR K <60654719+aswinkumar304@users.noreply.github.com>
2023-04-19 12:08:21 +0530

    Update sample_python_sql .py

--- sample_python_sql .py
+++ sample_python_sql .py
@@ -2,8 +2,8 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="root",
-  password="1234",
+  user="****",
+  password="****",
   database="sportsanalyst"
 )
 
